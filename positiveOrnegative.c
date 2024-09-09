d3141e1e30a957fbe35859afcc69bf5bf795c0f1
farhadislamhridoy <farhadislamhridoy@gmail.com>
2024-09-09 22:52:31 +0600

    Add files via upload
    Checking n(variable) is positive or negative in c

---
+++ positiveOrnegative.c
@@ -0,0 +1,17 @@
+// checking if n is a positive number or a nagetive number
+#include <stdio.h>
+
+int main()
+{
+int n;
+    n = 10; 
+    if(n >= 0) 
+        {
+            printf("The number is positive\n");
+        }
+    else 
+        {
+            printf("The number is negative\n");
+        }
+    return 0;
+}
